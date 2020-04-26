36a4c01484484d841cff690942130098d793e2b4
Philippe Timothe <philippetimothe@gmail.com>
2020-04-26 19:40:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,9 +65,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-  
-	programmer_hash = 
- 		{
+	programmer_hash = {
  		    :yukihiro_matsumoto => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
@@ -86,11 +84,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
-      
-   
-  
- return programmer_hash
+   return programmer_hash
 end
 
 def changing_alan
